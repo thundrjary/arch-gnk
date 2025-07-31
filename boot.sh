e65c6ba8c2f249df5303d28bc4d9ceecea5b9dfe
Jarom Later <jaromdl@gmail.com>
2025-07-31 15:32:54 -0600

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-ansi_art='                 ▄▄▄                                                   
- ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄ 
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   █▀   ███   ███  ███   ███
-███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄ ███▄▄▄███
-███   ███  ███   ███   ███ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███  ▀▀▀▀▀▀███
-███   ███  ███   ███   ███  ███   ███ ██████████  ███   █▄   ███   ███  ▄██   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
- ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀ 
-                                       ███   █▀                                  '
+ansi_art='
+ ██████╗ ███████╗███╗   ██╗████████╗██╗     ███████╗       ██╗       ██╗  ██╗██╗███╗   ██╗██████╗ 
+██╔════╝ ██╔════╝████╗  ██║╚══██╔══╝██║     ██╔════╝       ██║       ██║ ██╔╝██║████╗  ██║██╔══██╗
+██║  ███╗█████╗  ██╔██╗ ██║   ██║   ██║     █████╗      ████████╗    █████╔╝ ██║██╔██╗ ██║██║  ██║
+██║   ██║██╔══╝  ██║╚██╗██║   ██║   ██║     ██╔══╝      ██╔═██╔═╝    ██╔═██╗ ██║██║╚██╗██║██║  ██║
+╚██████╔╝███████╗██║ ╚████║   ██║   ███████╗███████╗    ██████║      ██║  ██╗██║██║ ╚████║██████╔╝
+ ╚═════╝ ╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚══════╝    ╚═════╝      ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ '
 
 clear
 echo -e "\n$ansi_art\n"
 
 sudo pacman -Sy --noconfirm --needed git
 
-echo -e "\nCloning Omarchy..."
-rm -rf ~/.local/share/omarchy/
-git clone https://github.com/basecamp/omarchy.git ~/.local/share/omarchy >/dev/null
+echo -e "\nCloning GNK..."
+rm -rf ~/.local/share/gnk/
+git clone https://github.com/basecamp/gnk.git ~/.local/share/gnk >/dev/null
 
 # Use custom branch if instructed
-if [[ -n "$OMARCHY_REF" ]]; then
-  echo -e "\eUsing branch: $OMARCHY_REF"
-  cd ~/.local/share/omarchy
-  git fetch origin "${OMARCHY_REF}" && git checkout "${OMARCHY_REF}"
+if [[ -n "$GNK_REF" ]]; then
+  echo -e "\eUsing branch: $GNK_REF"
+  cd ~/.local/share/gnk
+  git fetch origin "${GNK_REF}" && git checkout "${GNK_REF}"
   cd -
 fi
 
 echo -e "\nInstallation starting..."
-source ~/.local/share/omarchy/install.sh
+source ~/.local/share/gnk/install.sh
