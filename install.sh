93b61486e7ffd3e015550754e602f28d372bf123
Jarom Later <jaromdl@gmail.com>
2025-07-31 15:36:07 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-OMARCHY_INSTALL=~/.local/share/omarchy/install
+GNK_INSTALL=~/.local/share/GNK/install
 
 # Give people a chance to retry running the installation
 catch_errors() {
-  echo -e "\n\e[31mOmarchy installation failed!\e[0m"
-  echo "You can retry by running: bash ~/.local/share/omarchy/install.sh"
+  echo -e "\n\e[31mGNK installation failed!\e[0m"
+  echo "You can retry by running: bash ~/.local/share/GNK/install.sh"
   echo "Get help from the community: https://discord.gg/tXFUdasqhY"
 }
 
@@ -16,7 +16,7 @@ trap catch_errors ERR
 
 show_logo() {
   clear
-  tte -i ~/.local/share/omarchy/logo.txt --frame-rate ${2:-120} ${1:-expand}
+  tte -i ~/.local/share/GNK/logo.txt --frame-rate ${2:-120} ${1:-expand}
   echo
 }
 
@@ -26,49 +26,49 @@ show_subtext() {
 }
 
 # Install prerequisites
-source $OMARCHY_INSTALL/preflight/aur.sh
-source $OMARCHY_INSTALL/preflight/presentation.sh
+source $GNK_INSTALL/preflight/aur.sh
+source $GNK_INSTALL/preflight/presentation.sh
 
 # Configuration
 show_logo beams 240
-show_subtext "Let's install Omarchy! [1/5]"
-source $OMARCHY_INSTALL/config/identification.sh
-source $OMARCHY_INSTALL/config/config.sh
-source $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
-source $OMARCHY_INSTALL/config/fix-fkeys.sh
-source $OMARCHY_INSTALL/config/network.sh
-source $OMARCHY_INSTALL/config/power.sh
-source $OMARCHY_INSTALL/config/timezones.sh
-source $OMARCHY_INSTALL/config/login.sh
-source $OMARCHY_INSTALL/config/nvidia.sh
+show_subtext "Let's install GNK! [1/5]"
+source $GNK_INSTALL/config/identification.sh
+source $GNK_INSTALL/config/config.sh
+source $GNK_INSTALL/config/detect-keyboard-layout.sh
+source $GNK_INSTALL/config/fix-fkeys.sh
+source $GNK_INSTALL/config/network.sh
+source $GNK_INSTALL/config/power.sh
+source $GNK_INSTALL/config/timezones.sh
+source $GNK_INSTALL/config/login.sh
+source $GNK_INSTALL/config/nvidia.sh
 
 # Development
 show_logo decrypt 920
 show_subtext "Installing terminal tools [2/5]"
-source $OMARCHY_INSTALL/development/terminal.sh
-source $OMARCHY_INSTALL/development/development.sh
-source $OMARCHY_INSTALL/development/nvim.sh
-source $OMARCHY_INSTALL/development/ruby.sh
-source $OMARCHY_INSTALL/development/docker.sh
-source $OMARCHY_INSTALL/development/firewall.sh
+source $GNK_INSTALL/development/terminal.sh
+source $GNK_INSTALL/development/development.sh
+source $GNK_INSTALL/development/nvim.sh
+source $GNK_INSTALL/development/ruby.sh
+source $GNK_INSTALL/development/docker.sh
+source $GNK_INSTALL/development/firewall.sh
 
 # Desktop
 show_logo slice 60
 show_subtext "Installing desktop tools [3/5]"
-source $OMARCHY_INSTALL/desktop/desktop.sh
-source $OMARCHY_INSTALL/desktop/hyprlandia.sh
-source $OMARCHY_INSTALL/desktop/theme.sh
-source $OMARCHY_INSTALL/desktop/bluetooth.sh
-source $OMARCHY_INSTALL/desktop/asdcontrol.sh
-source $OMARCHY_INSTALL/desktop/fonts.sh
-source $OMARCHY_INSTALL/desktop/printer.sh
+source $GNK_INSTALL/desktop/desktop.sh
+source $GNK_INSTALL/desktop/hyprlandia.sh
+source $GNK_INSTALL/desktop/theme.sh
+source $GNK_INSTALL/desktop/bluetooth.sh
+source $GNK_INSTALL/desktop/asdcontrol.sh
+source $GNK_INSTALL/desktop/fonts.sh
+source $GNK_INSTALL/desktop/printer.sh
 
 # Apps
 show_logo expand
 show_subtext "Installing default applications [4/5]"
-source $OMARCHY_INSTALL/apps/webapps.sh
-source $OMARCHY_INSTALL/apps/xtras.sh
-source $OMARCHY_INSTALL/apps/mimetypes.sh
+source $GNK_INSTALL/apps/webapps.sh
+source $GNK_INSTALL/apps/xtras.sh
+source $GNK_INSTALL/apps/mimetypes.sh
 
 # Updates
 show_logo highlight
