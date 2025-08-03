e458fab4588de6c4434eff48922304b128818bfd
Jarom Later <jaromdl@gmail.com>
2025-08-03 16:36:40 -0600

    Update terminal.sh

--- install/development/terminal.sh
+++ install/development/terminal.sh
@@ -2,7 +2,7 @@
 
 yay -S --noconfirm --needed \
   wget curl unzip inetutils impala \
-  fd eza fzf ripgrep zoxide bat jq \ #TODO-JDL: jq not needed, others fine
+  fd eza fzf ripgrep zoxide bat jq \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate bash-completion \
-  alacritty #TODO-JDL: consider changing to kitty
+  alacritty
