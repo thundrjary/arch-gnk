0e2dfe3ddc833ef0446e6d0fd2c4c8635086a3d3
Jarom Later <jaromdl@gmail.com>
2025-08-03 15:44:08 -0600

    Update desktop.sh

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -6,7 +6,7 @@ yay -S --noconfirm --needed \
   nautilus sushi ffmpegthumbnailer \
   slurp satty \
   mpv evince imv \
-  chromium
+  chromium gedit
 
 # Add screen recorder based on GPU
 if lspci | grep -qi 'nvidia'; then
