c1b80bf4098ea6b53f92b90bae3642d87be573cd
Jarom Later <jaromdl@gmail.com>
2025-08-03 15:39:44 -0600

    Update kidsapps.sh

--- install/desktop/kidsapps.sh
+++ install/desktop/kidsapps.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 yay -S --noconfirm --needed \
-  steamcmd tuxpaint gcompris-qt
+  steamcmd tuxpaint gcompris-qt asciiquarium cowsay \
+  blinken klettres khangman kig kturtle minuet tuxtype \
+  supertux gbrainy kanagram minecraft-launcher kolf \
+  lightsoff pingus palapeli ri-li ktuberling ksame \
+  klickety vvvvvv-git moon-buggy neverball \
+  nikki kbounce frozen-bubble
