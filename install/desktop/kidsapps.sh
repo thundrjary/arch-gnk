45fdee8634f67ab5703ead4675add35edd0d6714
Jarom Later <jaromdl@gmail.com>
2025-08-03 16:03:59 -0600

    Update kidsapps.sh

--- install/desktop/kidsapps.sh
+++ install/desktop/kidsapps.sh
@@ -6,10 +6,15 @@ yay -S --noconfirm --needed \
   supertux gbrainy kanagram minecraft-launcher kolf \
   lightsoff pingus palapeli ri-li ktuberling ksame \
   klickety vvvvvv-git moon-buggy neverball \
-  nikki kbounce frozen-bubble
+  nikki kbounce frozen-bubble \
+  krita bookworm
 
 # terminal
 yay -S --noconfirm --needed \
   greed worms fortune cowsay asciiquarium ponysay \
   figlet sl bb pipes.sh tty-clock nyancan \
   aafire espeak beep
+
+# emulators
+yay -S --noconfirm --needed \
+  libretro-nestopia snes9x dolphin desmume mednafen dosbox
